664ab4570a53b3611f14871b20a39520a1e1aa04
arifs16 <arifsetyadi16@gmail.com>
2023-04-06 09:08:32 +0700

    Recent Indonesia Earthquake with package recent-indonesia-earthquake

--- main.py
+++ main.py
@@ -3,10 +3,9 @@ Aplikasi deteksi gempa terkini
 MODULARISASI DENGAN FUNCTION
 MODULARISASI DENGAN PACKAGES
 """
-
 import recentearthquake
 
 if __name__ == '__main__':
-    print('APLIKASI UTAMA')
+    print(f'Aplikasi utama menggunakan package yang memiliki deskripsi {recentearthquake.description}')
     result = recentearthquake.ekstraksi_data()
     recentearthquake.tampilkan_data(result)
